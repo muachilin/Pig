b6d38408d407b5a990d3843e07581bb92db244cb
muachilin <32566798+muachilin@users.noreply.github.com>
2020-04-08 22:02:57 +0800

    Add stdout debugging message

--- dist/index.js
+++ dist/index.js
@@ -3564,6 +3564,7 @@ function run() {
                     const leftParaIndex = res.data.title.indexOf('(');
                     const rightParaIndex = res.data.title.indexOf(')');
                     const linkIssueStr = res.data.title.substring(leftParaIndex + 1, rightParaIndex);
+                    process.stdout.write(`The parse issue number is:${linkIssueStr}\n`);
                     const linkIssueNumber = +linkIssueStr;
                     yield githubClient.issues.addLabels({
                         owner: github_1.context.repo.owner,
